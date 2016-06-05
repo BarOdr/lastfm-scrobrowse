f68e41d23e4a1b7f99df1a7f1e49f73d68d5c203
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-06-05 13:09:39 +0200

    configure mvc folders

---
+++ Pods/Target Support Files/Pods-lastfm-scrobrows/Pods-lastfm-scrobrows-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_lastfm_scrobrows : NSObject
+@end
+@implementation PodsDummy_Pods_lastfm_scrobrows
+@end
---
+++ Pods/Target Support Files/Pods-lastfm-scrobrows/Pods-lastfm-scrobrows-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_lastfm_scrobrowsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_lastfm_scrobrowsVersionString[];
+
